d0fde1b91bc6362bae211a106a11fc4fd9c6fbdc
RajatSharma <withrajatsharma@gmail.com>
2024-03-02 15:22:58 +0530

    enabled routing

--- frontend/index.js
+++ frontend/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
-   <App/>
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+

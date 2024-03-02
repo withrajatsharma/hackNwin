05000627453f639d18d52946b2476902c089d825
RajatSharma <withrajatsharma@gmail.com>
2024-03-02 15:45:58 +0530

    update

--- frontend/src/components/Searchbar.js
+++ frontend/src/components/Searchbar.js
@@ -1,6 +1,6 @@
 import React,{useState,  useEffect} from "react";
 import { useNavigate } from "react-router-dom";
-import Navbar from './Navbar';
+import Navbar from '../components/Navbar';
 
 const Searchbar = ({token}) =>{
 

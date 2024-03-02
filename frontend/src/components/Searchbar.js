86da894d8462ff3f852916e97c378ac577839de7
RajatSharma <withrajatsharma@gmail.com>
2024-03-02 15:34:16 +0530

    update folder structure

--- frontend/src/components/Searchbar.js
+++ frontend/src/components/Searchbar.js
@@ -1,6 +1,6 @@
 import React,{useState,  useEffect} from "react";
 import { useNavigate } from "react-router-dom";
-import Navbar from '../components/Navbar';
+import Navbar from './Navbar';
 
 const Searchbar = ({token}) =>{
 

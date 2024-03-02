b7428a229c95f27bdfe7bc1b1bd72002748a9137
RajatSharma <withrajatsharma@gmail.com>
2024-03-02 15:21:47 +0530

    frontend setup

---
+++ frontend/App.js
@@ -0,0 +1,16 @@
+import React,{useState, useEffect} from 'react';
+import './App.css';
+
+
+function App() {
+
+
+
+  return(
+    <div className=''>
+    
+    </div>
+  );
+}
+
+export default App;
